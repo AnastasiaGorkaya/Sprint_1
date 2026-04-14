45c4c55c697c9a267936c8c50d3b5cdc08ee3f5b
AnastasiaR <den.zimny@yandex.ru>
2026-04-14 18:52:33 +0300

    Add правки for PR

--- Sprint_1/task_2.py
+++ Sprint_1/task_2.py
@@ -14,4 +14,4 @@ class Tester:
 tester_1 = Tester(name="tester_1")
 tester_1.work_hard(deadline=False)  # 'tester_1 Можно отдыхать'
 tester_2 = Tester(name='tester_2')
-tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!'
+tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!' 
